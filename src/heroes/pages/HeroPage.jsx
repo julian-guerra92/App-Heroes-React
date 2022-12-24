a7fb43bbd551882f796b87cd5d0db303dbfa3a86
Julian Guerra <julianguerra92@gmail.com>
2022-12-24 11:00:39 -0500

    Creación del proyecto. En este commit ya se cuenta con estructura de rutas y renderización de páginas para búsqueda de heroes y por publiador

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,44 @@
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
+
+export const HeroPage = () => {
+
+   const { heroId } = useParams();
+   const navigate = useNavigate();
+   const hero = useMemo(() => getHeroById(heroId), [heroId]);
+
+   const onNavigateBack = () => {
+      navigate(-1);
+   }
+
+   if (!hero) return <Navigate to="/marvel" />;
+
+   return (
+      <div className="row mt-5">
+         <div className="col-4">
+            <img
+               src={`/assets/heroes/${heroId}.jpg`}
+               alt={hero.superhero}
+               className="img-thumbnail animate__animated animate__fadeInLeft"
+            />
+         </div>
+         <div className="col-8">
+            <h2>{hero.superhero}</h2>
+            <ul className="list-group list-group-flush">
+               <li className="list-group-item"><b>Alter ego: </b>{hero.alter_ego}</li>
+               <li className="list-group-item"><b>Publiser: </b>{hero.publisher}</li>
+               <li className="list-group-item"><b>First appearance: </b>{hero.first_appearance}</li>
+            </ul>
+            <h3>Characters</h3>
+            <p>{hero.characters}</p>
+            <button
+               className="btn btn-outline-primary"
+               onClick={onNavigateBack}
+            >
+               Return
+            </button>
+         </div>
+      </div>
+   )
+}
